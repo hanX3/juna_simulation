330f117dc8923d248c0e51460707ddcd5dfc87d6
hanX <hanxingchi@gmail.com>
2022-02-04 17:15:57 +0800

    add 2020 320keV analysis

---
+++ analysis/fit/2020/320keV/fall_part/macro_analysis.cpp
@@ -0,0 +1,28 @@
+{
+  gROOT->ProcessLine(".!rm -f ../../../../../../result/2020/320keV/chi2_fall_part.dat");
+
+  gROOT->ProcessLine(".L analysis.cpp");
+  gROOT->ProcessLine("analysis(317.8)");
+  gROOT->ProcessLine("analysis(317.9)");
+  gROOT->ProcessLine("analysis(318.0)");
+  gROOT->ProcessLine("analysis(318.1)");
+  gROOT->ProcessLine("analysis(318.2)");
+  gROOT->ProcessLine("analysis(318.3)");
+  gROOT->ProcessLine("analysis(318.4)");
+  gROOT->ProcessLine("analysis(318.5)");
+  gROOT->ProcessLine("analysis(318.6)");
+  gROOT->ProcessLine("analysis(318.7)");
+  gROOT->ProcessLine("analysis(318.8)");
+  gROOT->ProcessLine("analysis(318.9)");
+  gROOT->ProcessLine("analysis(319.0)");
+  gROOT->ProcessLine("analysis(319.1)");
+  gROOT->ProcessLine("analysis(319.2)");
+  gROOT->ProcessLine("analysis(319.3)");
+  gROOT->ProcessLine("analysis(319.4)");
+  gROOT->ProcessLine("analysis(319.5)");
+  gROOT->ProcessLine("analysis(319.6)");
+  gROOT->ProcessLine("analysis(319.7)");
+  gROOT->ProcessLine("analysis(319.8)");
+
+  gROOT->ProcessLine(".q");
+}
